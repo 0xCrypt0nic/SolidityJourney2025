de40deffa824bad72885be2d4ee4ef18d46396c1
0xCrypt0nic <n.greneche45@gmail.com>
2025-08-11 19:50:57 +0200

    NodeJS basic components review

--- weeks/week1/day1_nodeJS_update/app.js
+++ weeks/week1/day1_nodeJS_update/app.js
@@ -1,6 +1,12 @@
 // console.log("Hello, Developers !");
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
+const os = require("os");
+const url = require("url");
+const crypto = require("crypto");
+
+// ===== FS SECTION ===== //
 
 // fs.readFile("./example.txt", "utf-8", (err, data) => {
 //   if (err) {
@@ -21,12 +27,43 @@ const http = require("http");
 //   console.log('File written successfully');
 // });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World");
-});
+// ===== HTTP SECTION ===== //
 
-server.listen(3000, () => {
-  console.log("Server runing at http://localhost:3000");
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello, World");
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server runing at http://localhost:3000");
+// });
+
+// ===== PATH SECTION ===== //
+
+// const directory = "./user/local";
+// const fileName = "example.tkt";
+
+// const fullPath = path.join(directory, fileName);
+
+// console.log(fullPath);
+
+// ===== OS SECTION ===== //
+
+// console.log("Platform : ", os.platform());
+// console.log("CPU Arch : ", os.arch());
+// console.log("Total Memory : ", os.totalmem());
+// console.log("Free Memory : ", os.freemem());
+
+// ===== URL SECTION ===== //
+
+// const myURL = new URL("https://example.com:8080/path/name?query=hello#hash");
+// console.log("Host : ", myURL.host);
+// console.log("PathName : ", myURL.pathname);
+// console.log("Search Params : ", myURL.searchParams.get("query"));
+
+// ===== CRYPTO SECTION ===== //
+
+// const hash = crypto.createHash("sha256");
+// hash.update("Hello World");
+// console.log(hash.digest("hex"));

fd652a86276e7d2f8201a0e741c0d6f9c2ef449c
0xCrypt0nic <n.greneche45@gmail.com>
2025-08-15 14:46:51 +0200

    Streams from fs module

--- weeks/week1/day1_nodeJS_update/app.js
+++ weeks/week1/day1_nodeJS_update/app.js
@@ -79,6 +79,31 @@ const lodash = require("lodash");
 // console.log(`5 - 4 = ${math.subtract(5, 4)}`);
 
 // ===== NPM PACKAGE MANAGER ===== //
-const numbers = [1, 2, 3, 4, 5];
-const reversed = lodash.reverse(numbers);
-console.log(reversed);
+// const numbers = [1, 2, 3, 4, 5];
+// const reversed = lodash.reverse(numbers);
+// console.log(reversed);
+
+// ===== STREAM ===== //
+// const readableStream = fs.createReadStream("./example.txt", {
+//   encoding: "utf-8",
+// });
+
+// readableStream.on("data", (chunk) => {
+//   console.log(chunk);
+// });
+
+// readableStream.on("end", () => {
+//   console.log("Finished reading the file.");
+// });
+
+// readableStream.on("error", (er) => {
+//   console.error("Error", err);
+// });
+
+const writableStream = fs.createWriteStream("./output2.txt");
+writableStream.write("Hello, ");
+writableStream.write("World !");
+writableStream.end();
+writableStream.on("finish", () => {
+  console.log("Finished writing to the file.");
+});

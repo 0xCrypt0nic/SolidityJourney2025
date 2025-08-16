3d27f403a6efdb5f431dcec792647c83c49f5e91
0xCrypt0nic <n.greneche45@gmail.com>
2025-08-16 13:01:14 +0200

    Routes module intro

--- weeks/week1/day1_nodeJS_update/server.js
+++ weeks/week1/day1_nodeJS_update/server.js
@@ -27,20 +27,62 @@ const url = require("url");
 //   }
 // });
 
+// ==================== Route Handler ==================== //
+
+// 1. Routes in a Simple Way
+
+// const routes = {
+//   "/": (req, res) => {
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("Welcome to Homepage");
+//   },
+//   "/about": (req, res) => {
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("This is the About Page");
+//   },
+//   "/notfound": (req, res) => {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Page Not Found");
+//   },
+// };
+
+// const server = http.createServer((req, res) => {
+//   const { pathname } = url.parse(req.url);
+//   if (routes[pathname]) {
+//     routes[pathname](req, res);
+//   } else {
+//     routes["/notfound"](req, res);
+//   }
+// });
+
+// 2. Handling Dynamic Routes (website.com)
+
 const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url.startsWith("/search")) {
-    const queryObject = url.parse(req.url, true).query;
-    res.writeHead(200, {
-      "content-type": "application/json",
-      "custom-header": "Node JS Server",
-      "custom-tracking": "1234",
-    });
-    res.end(JSON.stringify({ message: "Query received", queryObject }));
+  const { pathname } = url.parse(req.url);
+  if (pathname.startsWith("/user/")) {
+    const userId = pathname.split("/")[2];
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end(`User ID : ${userId}`);
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
     res.end("Route Not Found");
   }
 });
+
+// const server = http.createServer((req, res) => {
+//   if (req.method === "GET" && req.url.startsWith("/search")) {
+//     const queryObject = url.parse(req.url, true).query;
+//     res.writeHead(200, {
+//       "content-type": "application/json",
+//       "custom-header": "Node JS Server",
+//       "custom-tracking": "1234",
+//     });
+//     res.end(JSON.stringify({ message: "Query received", queryObject }));
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("Route Not Found");
+//   }
+// });
 
 const PORT = 3000;
 server.listen(PORT, () => {

15473ec59443b8608632aea1094c7ad4a23e77f1
0xCrypt0nic <n.greneche45@gmail.com>
2025-10-02 16:02:08 +0200

    day 3 finished

--- weeks/week8/day3/VotingProject/hardhat.config.ts
+++ weeks/week8/day3/VotingProject/hardhat.config.ts
@@ -8,6 +8,9 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
+      hardhat: {
+        chainId: 31337
+      },
       sepolia: {
           url: "https://sepolia.infura.io/v3/a19cc6549e95446db6d8e58fc3eed3fe",
           accounts: [process.env.PRIVATE_KEY || ""]
